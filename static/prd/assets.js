275de11c9bdb7f5dcde230455d77f8f64ae56114
yanhao <136393414@qq.com>
2017-12-22 21:27:06 +0800

    chore: rebuild

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@3b0267e52c1972c326f5.js","css":"index@3b0267e52c1972c326f5.css"},"lib":{"js":"lib@8944c3e5df6f7a53e2a6.js"},"lib2":{"js":"lib2@f60b8bd0c5cee11ac9fd.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@42604f2cc691a3711b65.js","css":"index@42604f2cc691a3711b65.css"},"lib":{"js":"lib@c61fbff9d024e5ee3e35.js"},"lib2":{"js":"lib2@d0cf62a45154fd309066.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
